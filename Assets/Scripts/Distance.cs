7479d44605f815147305b8e8b8f7ed7e650d53f6
Alicetear <phrnakanth@gmail.com>
2026-05-01 02:51:23 +0700

    Edit Script distance

--- Assets/Scripts/Distance.cs
+++ Assets/Scripts/Distance.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 public class Distance : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI distanceText;
+    [SerializeField] PlayerController player;
     private float totalDistance = 0f;
+    private bool isGameOver = false;
 
     void Start()
     {
         if (distanceText != null)
+        {
             distanceText.gameObject.SetActive(false);
+        }
     }
 
     void Update()
     {
-        if (Time.timeScale == 0f) return;
+        if (Time.timeScale == 0f)
+        {
+            return;
+        }
+        if (player == null || player.gameOver)
+        {
+            return; 
+        }
 
         if (!distanceText.gameObject.activeSelf)
+        {
             distanceText.gameObject.SetActive(true);
+        }
 
         totalDistance += Time.deltaTime * 10f;
         distanceText.text = "Distance " + totalDistance.ToString("F1") + " Meter";

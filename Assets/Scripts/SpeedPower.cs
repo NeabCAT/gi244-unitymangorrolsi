02d534d3c40daa4ee2eb018c7457dee2a1334476
MalineeSunrise <malineebukong@gmail.com>
2026-04-30 23:32:43 +0700

    SpeedPower

--- Assets/Scripts/SpeedPower.cs
+++ Assets/Scripts/SpeedPower.cs
@@ -1,21 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpeedPower : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private PlayerController player;
 
     void OnTriggerEnter(Collider other)
     {
-        
+        if (!other.CompareTag("Player")) return;
+        player = other.GetComponent<PlayerController>();
+
+        StartCoroutine(Boost());
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    IEnumerator Boost()
+    {
+
+        MoveLeft.speedBoost = 2f;
+        player.noDamage = true;
+        yield return new WaitForSeconds(5);
+
+        MoveLeft.speedBoost = 1f;
+        player.noDamage = false;
+        gameObject.SetActive(false);
     }
 }

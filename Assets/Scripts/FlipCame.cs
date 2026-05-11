9cd21e6a816d403ab3e76f96c9a07a71b8755a5f
MalineeSunrise <malineebukong@gmail.com>
2026-05-11 22:46:39 +0700

    flipcame

--- Assets/Scripts/FlipCame.cs
+++ Assets/Scripts/FlipCame.cs
@@ -1,29 +1,59 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UIElements;
 
 public class FlipCame : MonoBehaviour
 {
+    private static bool isFlipping = false;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            if (isFlipping) return;
+
             var player = other.GetComponent<PlayerController>();
-
-            player.StartCoroutine(FlipEffect(player.cam.transform));
-
+            player.StartCoroutine(FlipEffect(player.cam.transform, player));
             ItemPool.Instance.Return(gameObject);
         }
     }
 
-    IEnumerator FlipEffect(Transform cam)
+    IEnumerator FlipEffect(Transform cam, PlayerController player)
     {
+        isFlipping = true;
+
+        // บอกก่อนหมุน
+        for (int i = 3; i >= 1; i--)
+        {
+            player.countdownText.text = i.ToString();
+            player.countdownText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1f);
+        }
+        player.countdownText.text = "Flip!";
+        yield return new WaitForSeconds(1f);
+        player.countdownText.gameObject.SetActive(false);
+
+        // หมุนกล้อง
         Quaternion startRot = cam.localRotation;
         cam.localRotation = Quaternion.Euler(0, 0, 180f);
 
         yield return new WaitForSeconds(5f);
 
+        // บอกก่อนกลับปกติ
+        for (int i = 3; i >= 1; i--)
+        {
+            player.countdownText.text = i.ToString();
+            player.countdownText.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1f);
+        }
+        player.countdownText.gameObject.SetActive(false);
+
+        // กลับปกติ
         if (cam != null)
             cam.localRotation = startRot;
+
+        isFlipping = false;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -29,6 +30,7 @@ public class PlayerController : MonoBehaviour
     //public bool isDash = false;
 
     public GameObject cam;
+    public TextMeshProUGUI countdownText;
 
     void Awake()
     {
